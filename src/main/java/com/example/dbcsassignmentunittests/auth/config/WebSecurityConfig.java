4e4f276603ebdbcf70c3d18e63dd92cb25fc5ba2
Wiliam Toader <wiliamtoader@protonmail.com>
2022-04-26 05:49:53 +0300

    Jwt kinda done

--- src/main/java/com/example/dbcsassignmentunittests/auth/config/WebSecurityConfig.java
+++ src/main/java/com/example/dbcsassignmentunittests/auth/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.dbcsassignmentunittests.auth.config;
 
 import com.example.dbcsassignmentunittests.auth.filters.JwtRequestFilter;
+import com.example.dbcsassignmentunittests.auth.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,16 +19,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtRequestFilter jwtRequestFilter;
-
-    public WebSecurityConfig(JwtRequestFilter jwtRequestFilter) {
+    private final JwtUtil jwtUtil;
+    public WebSecurityConfig(JwtRequestFilter jwtRequestFilter, JwtUtil jwtUtil) {
         this.jwtRequestFilter = jwtRequestFilter;
+        this.jwtUtil = jwtUtil;
     }
+
+
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -38,7 +50,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/auth", "/register", "/data/user", "/bps/**", "/email", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll().
                 anyRequest().authenticated().and().
                 exceptionHandling().and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().formLogin().successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.getWriter().append(jwtUtil.generateToken((String) authentication.getPrincipal()));
+                    }
+                });
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
     }
